ebfcd6b6de4778e42c54472941c80c219266852a
MillerKites <mtkites42@tntech.edu>
2026-04-16 22:17:12 -0500

    Refactor A* algorithm types and namespace

--- code/a_star.h
+++ code/a_star.h
@@ -1,20 +1,20 @@
-// Source - https://codereview.stackexchange.com/q/285421
-// Posted by Green 绿色, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-04-16, License - CC BY-SA 4.0
-
+// a_star.h
+#pragma once
 #include <functional>
 #include <map>
+#include <unordered_map>
 #include <vector>
 
-typedef size_t NodeID;
-typedef float Cost;
-typedef std::pair<const NodeID, Cost> Edge;
-typedef std::map<const NodeID, std::map<const NodeID, Cost>> AdjacencyList;
-typedef std::vector<NodeID> Path;
-typedef const std::function<Cost(NodeID, NodeID)> HeuristicFn;
+namespace astar {
+    typedef size_t NodeID;
+    typedef float Cost;
+    typedef std::pair<const NodeID, Cost> Edge;
+    typedef std::unordered_map<NodeID, std::unordered_map<NodeID, Cost>> AdjacencyList;
+    typedef std::vector<NodeID> Path;
+    typedef const std::function<Cost(NodeID, NodeID)> HeuristicFn;
 
-
-Path a_star(AdjacencyList& adjacency,
-            NodeID         start,
-            NodeID         target,
-            HeuristicFn&   heuristic);
+    Path a_star(AdjacencyList& adjacency,
+                NodeID start,
+                NodeID target,
+                HeuristicFn& heuristic);
+}
