fdae50e1ac9ecfa4081a6a7274cbd13af238f730
Franklin <fjsintic42@tntech.edu>
2026-04-17 22:02:24 -0500

    Update bellman_ford.cpp

--- code/bellman_ford.cpp
+++ code/bellman_ford.cpp
@@ -1,58 +1,42 @@
-
-// C++ program for implementing the Bellman-Ford algorithm
-
 #include <iostream>
-#include <limits.h>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 namespace bellmanford {
-    // Structure to represent an edge in the graph
+
     struct Edge {
-        int source, destination, weight;
+        int source;
+        int destination;
+        int weight;
     };
 
-    // Function to run the Bellman-Ford algorithm
-    void bellmanFord(vector<Edge>& graph, int vertices,
-                    int source)
+    vector<int> bellmanFord(vector<Edge>& graph, int vertices, int source)
     {
-        // Initialize distances from source to all vertices as
-        // INFINITE
         vector<int> distance(vertices, INT_MAX);
-        distance[source] = 0; // Distance to source itself is 0
+        distance[source] = 0;
 
-        // Relax all edges |V| - 1 times (where |V| is the
-        // number of vertices)
-        for (int i = 0; i < vertices - 1; ++i) {
-            // Iterate through all edges
+        for (int i = 0; i < vertices - 1; i++) {
             for (const Edge& e : graph) {
-                // Update distances if a shorter path is found
-                if (distance[e.source] != INT_MAX
-                    && distance[e.source] + e.weight
-                        < distance[e.destination]) {
-                    distance[e.destination]
-                        = distance[e.source] + e.weight;
+                if (distance[e.source] != INT_MAX &&
+                    distance[e.source] + e.weight < distance[e.destination]) {
+
+                    distance[e.destination] =
+                        distance[e.source] + e.weight;
                 }
             }
         }
 
-        // Check for negative-weight cycles by iterating through
-        // all edges again
         for (const Edge& e : graph) {
-            if (distance[e.source] != INT_MAX
-                && distance[e.source] + e.weight
-                    < distance[e.destination]) {
-                cout
-                    << "Graph contains negative weight cycle\n";
-                return;
+            if (distance[e.source] != INT_MAX &&
+                distance[e.source] + e.weight < distance[e.destination]) {
+                cout << "Graph contains negative weight cycle\n";
+                return {};
             }
         }
 
-        // Print shortest distances from source to all vertices
-        /*cout << "Vertex   Distance from Source\n";
-        for (int i = 0; i < vertices; ++i) {
-            cout << i << "\t\t" << distance[i] << "\n";
-        }*/
+        return distance;
     }
+
 }
