17e0db9216575661ceabe6cadb1251eeaa4b1786
Franklin <fjsintic42@tntech.edu>
2026-04-17 22:02:52 -0500

    Update graph_generator.cpp

--- code/graph_generator.cpp
+++ code/graph_generator.cpp
@@ -17,7 +17,7 @@ bool edgeExists(const vector<vector<pair<int,int>>>& graph, int u, int v) {
 // Function to generate a random weighted graph
 vector<vector<pair<int, int>>> generateGraph(int V, int E) {
     vector<vector<pair<int, int>>> graph(V);
-   
+
     int maxEdges = V * (V - 1) / 2;
 
     if (E > maxEdges) {
@@ -25,19 +25,32 @@ vector<vector<pair<int, int>>> generateGraph(int V, int E) {
         E = maxEdges;
     }
 
-    srand(time(0)); // seed for randomness
-    
-    for (int i = 0; i < E; i++) {
+    srand(time(0));
+
+    // Create a spanning tree (connect all nodes)
+    for (int i = 1; i < V; i++) {
+        int parent = rand() % i; // connect to any previous node
+        int weight = rand() % 30 + 1;
+
+        graph[i].push_back({parent, weight});
+        graph[parent].push_back({i, weight});
+    }
+
+    int edgesAdded = V - 1;
+
+    // Add remaining edges randomly
+    while (edgesAdded < E) {
         int u = rand() % V;
         int v = rand() % V;
 
-        // avoid self-loops
         if (u == v || edgeExists(graph, u, v)) continue;
 
-        int weight = rand() % 30 + 1; // weights from 1 to 10
+        int weight = rand() % 30 + 1;
 
         graph[u].push_back({v, weight});
-        graph[v].push_back({u, weight}); // remove this if directed graph
+        graph[v].push_back({u, weight});
+
+        edgesAdded++;
     }
 
     return graph;
@@ -53,3 +66,4 @@ void printGraph(const vector<vector<pair<int, int>>>& graph) {
         cout << endl;
     }
 }
+    
